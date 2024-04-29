0270d738f80e13de431c97ab666781d06f482fd5
Fayez Nazzal <soulldev@gmail.com>
2024-04-29 06:37:09 +0300

    Resort labels

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,4 @@
-import { PLATFORM_TYPE } from "./constants";
+import { KEY_LABELS, PLATFORM_TYPE } from "./constants";
 
 export const isMac = () => navigator.platform.includes(PLATFORM_TYPE.MAC);
 
@@ -13,12 +13,10 @@ export const isLetter = (key: string) => key.length === 1 && key.match(/[a-z]/i)
 export const getSortedKeys = (pressedKeys: Set<string>) => {
   const keys = Array.from(pressedKeys);
   keys.sort((a, b) => {
-    if (a === "ctrl") return -1;
-    if (b === "ctrl") return 1;
-    if (a === "shift") return -1;
-    if (b === "shift") return 1;
-    if (a === "alt") return -1;
-    if (b === "alt") return 1;
+    if (KEY_LABELS[a] || KEY_LABELS[`${a}_mac`]) return -1;
+    if (KEY_LABELS[b] || KEY_LABELS[`${b}_mac`]) return 1;
+    if (isLetter(a) && !isLetter(b)) return -1;
+    if (!isLetter(a) && isLetter(b)) return 1;
     return a.localeCompare(b);
   });
   return keys;
